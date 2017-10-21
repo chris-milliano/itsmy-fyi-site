41efc243053af6eff009ce218d9408a528494bee
Chris Milliano <MillianoC@gmail.com>
2017-10-21 00:23:03 -0500

    FEAT: Init app with router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const appRoutes: Routes = [
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/sign-up', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
