88fbb1394507a64a16e35040639411045532b821
Chris Milliano <MillianoC@gmail.com>
2017-10-22 13:34:43 -0500

    FEAT: Add construction page on sign up page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        console.log("Init header.component");
+    }
 
+    // Redirect to a different page
+    redirectTo (path: string) {
+        this.router.navigateByUrl(path);
+    }
 }
